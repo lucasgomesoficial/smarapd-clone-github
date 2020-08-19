4f130fda9ff3fb37fa3c379df6fc637b3d03a650
Lucas Gomes <lucasgomesbr@gmail.com>
2020-08-19 16:38:32 -0300

    Updates

---
+++ precache-manifest.a188df1cd9ff6e5bf0ec0d61149d326a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e046d75dc954fd1d9bf0d52e75eef9",
+    "url": "/smarapd-clone-github/index.html"
+  },
+  {
+    "revision": "c2a4b81aa11e2f17f513",
+    "url": "/smarapd-clone-github/static/css/2.32a52a2a.chunk.css"
+  },
+  {
+    "revision": "c2a4b81aa11e2f17f513",
+    "url": "/smarapd-clone-github/static/js/2.aa68ef78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smarapd-clone-github/static/js/2.aa68ef78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8575c67e38c99ee2f2",
+    "url": "/smarapd-clone-github/static/js/main.81903952.chunk.js"
+  },
+  {
+    "revision": "830ea4ba2b4d56d07c21",
+    "url": "/smarapd-clone-github/static/js/runtime-main.f146ec86.js"
+  }
+]);
